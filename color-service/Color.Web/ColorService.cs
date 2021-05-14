9e76e5c834313744c7d8ab389a9bb03a8b9df28b
Ruben Mertens <ruben.mertens@infosupport.com>
2021-05-14 14:33:16 +0200

    init

---
+++ color-service/Color.Web/ColorService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Color.Web
+{
+    public class ColorService
+    {
+        private readonly IConfiguration _configuration;
+
+        public ColorService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            var rn = new Random();
+            var colors = configuration.GetSection("Colors").Get<string[]>();
+
+            Color = colors[rn.Next(colors.Length - 1)];
+        }
+        
+        
+        public string Color { get; }
+    }
+}
---
+++ color-service/Color.Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Color.Web.Models;
+
+namespace Color.Web.Controllers
+{
+    public class 
+        HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ColorService _colorService;
+
+        public HomeController(ILogger<HomeController> logger, ColorService colorService)
+        {
+            _logger = logger;
+            _colorService = colorService;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Color = _colorService.Color;
+            return View();
+        }
+    }
+}
