3910b06842c087117e490ab20501935a15b2763c
Ruben Mertens <ruben.mertens@infosupport.com>
2021-05-14 16:11:12 +0200

    single service deploy

--- color-service/Color.Web/Controllers/HomeController.cs
+++ color-service/Color.Web/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Color.Web.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace Color.Web.Controllers
 {
@@ -14,15 +15,18 @@ namespace Color.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ColorService _colorService;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger, ColorService colorService)
+        public HomeController(ILogger<HomeController> logger, ColorService colorService, IConfiguration configuration)
         {
             _logger = logger;
             _colorService = colorService;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            ViewBag.Name = _configuration["POD_NAME"];
             ViewBag.Color = _colorService.Color;
             return View();
         }
--- kubernetes/k3d.ps1
+++ kubernetes/k3d.ps1
@@ -1,3 +1,2 @@
-
-k3d cluster create talk --registry-create
+k3d cluster create talk --registry-use k3d-local -p 30000:30000@server[0]
 kubectl config use-context k3d-talk
